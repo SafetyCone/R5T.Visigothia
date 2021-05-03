b7d7634671fa6b1610af64419612d4ded5784efa
David Coats <david@getsafetycone.com>
2021-05-03 13:47:06 -0700

    Add R5T.Visigothia.Default service implementation library and R5T.Visigothia.Standard service aggregation library.

---
+++ source/R5T.Visigothia.Default/Code/Services/Implementations/UserProfileDirectoryPathProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.Visigothia.Default
+{
+    public class UserProfileDirectoryPathProvider : IUserProfileDirectoryPathProvider
+    {
+        public Task<string> GetUserProfileDirectoryPath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/R5T.Visigothia.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Visigothia.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="UserProfileDirectoryPathProvider"/> implementation of <see cref="IUserProfileDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddUserProfileDirectoryPathProvider(this IServiceCollection services)
+        {
+            Default.IServiceCollectionExtensions.AddUserProfileDirectoryPathProvider(services);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="UserProfileDirectoryPathProvider"/> implementation of <see cref="IUserProfileDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IUserProfileDirectoryPathProvider> AddUserProfileDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<IUserProfileDirectoryPathProvider>(() => services.AddUserProfileDirectoryPathProvider());
+            return serviceAction;
+        }
+    }
+}
